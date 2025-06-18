708c0b0cbefbbf010231b379f642c416e90b4aa7
rigon <2412789408@qq.com>
2025-06-18 19:49:31 +0800

    feat: add session part

--- api/v1/session_controller.go
+++ api/v1/session_controller.go
@@ -1,18 +1,85 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"rigon-chat-server/internal/dto/request"
+	"rigon-chat-server/internal/service/gorm"
+	"rigon-chat-server/pkg/constants"
+	"rigon-chat-server/pkg/zlog"
+)
 
 // OpenSession 打开会话
-func OpenSession(c *gin.Context) {}
+func OpenSession(c *gin.Context) {
+	var openSessionReq request.OpenSessionRequest
+	if err := c.BindJSON(&openSessionReq); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, sessionId, ret := gorm.SessionService.OpenSession(openSessionReq)
+	JSONBack(c, message, ret, sessionId)
+}
 
 // GetUserSessionList 获取用户会话列表
-func GetUserSessionList(c *gin.Context) {}
+func GetUserSessionList(c *gin.Context) {
+	var getUserSessionListReq request.OwnlistRequest
+	if err := c.BindJSON(&getUserSessionListReq); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, sessionList, ret := gorm.SessionService.GetUserSessionList(getUserSessionListReq.OwnerId)
+	JSONBack(c, message, ret, sessionList)
+}
 
 // GetGroupSessionList 获取群会话列表
-func GetGroupSessionList(c *gin.Context) {}
+func GetGroupSessionList(c *gin.Context) {
+	var getGroupListReq request.OwnlistRequest
+	if err := c.BindJSON(&getGroupListReq); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, groupList, ret := gorm.SessionService.GetGroupSessionList(getGroupListReq.OwnerId)
+	JSONBack(c, message, ret, groupList)
+}
 
 // DeleteSession 删除会话
-func DeleteSession(c *gin.Context) {}
+func DeleteSession(c *gin.Context) {
+	var deleteSessionReq request.DeleteSessionRequest
+	if err := c.BindJSON(&deleteSessionReq); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, ret := gorm.SessionService.DeleteSession(deleteSessionReq.OwnerId, deleteSessionReq.SessionId)
+	JSONBack(c, message, ret, nil)
+}
 
 // CheckOpenSessionAllowed 检查用户是否允许打开会话
-func CheckOpenSessionAllowed(c *gin.Context) {}
+func CheckOpenSessionAllowed(c *gin.Context) {
+	var req request.CreateSessionRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, res, ret := gorm.SessionService.CheckOpenSessionAllowed(req.SendId, req.ReceiveId)
+	JSONBack(c, message, ret, res)
+}
