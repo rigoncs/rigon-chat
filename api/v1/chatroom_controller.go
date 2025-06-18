065e78574f4289a2105cb82f996da8f2aa14744d
rigon <2412789408@qq.com>
2025-06-18 21:33:42 +0800

    feat: add chatroom part

--- api/v1/chatroom_controller.go
+++ api/v1/chatroom_controller.go
@@ -1,8 +1,25 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"rigon-chat-server/internal/dto/request"
+	"rigon-chat-server/internal/service/gorm"
+	"rigon-chat-server/pkg/constants"
+	"rigon-chat-server/pkg/zlog"
+)
 
 // GetCurContactListInChatRoom 获取当前聊天室联系人列表
 func GetCurContactListInChatRoom(c *gin.Context) {
-	
+	var req request.GetCurContactListInChatRoomRequest
+	if err := c.BindJSON(&req); err != nil {
+		zlog.Error(err.Error())
+		c.JSON(http.StatusOK, gin.H{
+			"code":    500,
+			"message": constants.SYSTEM_ERROR,
+		})
+		return
+	}
+	message, rspList, ret := gorm.ChatRoomService.GetCurContactListInChatRoom(req.OwnerId, req.ContactId)
+	JSONBack(c, message, ret, rspList)
 }
